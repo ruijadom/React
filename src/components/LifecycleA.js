ffc274077b817e46cfb3e8c35a88c541350b74b0
ruijadom <ruijadomingues@gmail.com>
2019-11-10 14:57:45 +0000

    componentDidUpdate

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -20,11 +20,32 @@ export class LifecycleA extends Component {
     console.log("LifecycleA componentDidMount");
   }
 
+  shouldComponentUpdate() {
+    console.log("LifecycleA shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("LifecycleA getSnapshotBeforeUpdate");
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log("LifecycleA componentDidUpdate");
+  }
+
+  changeState = () => {
+    this.setState({
+      name: "Peter"
+    });
+  };
+
   render() {
     console.log("LifecycleA render");
     return (
       <div>
         <div>LifecycleA</div>
+        <button onClick={this.changeState}>change state</button>
         <LifecycleB></LifecycleB>
       </div>
     );
