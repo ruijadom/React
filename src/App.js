ded0b43568535098f5d3f87a7ed0d68904f50551
ruijadom <ruijadomingues@gmail.com>
2019-12-01 20:34:58 +0000

    HOF

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from "react";
 import "./App.css";
-import LifecycleA from "./components/LifecycleA";
-import FormComponent from "./components/FormComponent";
-import PureComponent from "./components/PureComponent";
-import ParentComp from "./components/ParentComponent";
-import RefsDemo from "./components/RefsDemo";
-import FRInput from "./components/FRInput";
-import FRParentInput from "./components/FRParentInput";
-import PortalDemo from "./components/PortalDemo";
-import Hero from "./components/Hero";
-import ErrorBoundary from "./components/ErrorBoundary";
+import HoverCounter from "./components/HoverCounter";
+import ClickCounter from "./components/ClickCounter";
 
 function App() {
   return (
     <div className="App">
-      {/*<FormComponent></FormComponent>*/}
-      {/*<LifecycleA></LifecycleA>*/}
-      {/*<ParentComp></ParentComp>*/}
-      {/*<RefsDemo></RefsDemo>*/}
-      {/*<FRParentInput></FRParentInput>*/}
-      {/*<PortalDemo></PortalDemo>*/}
-      <ErrorBoundary>
-        <Hero heroName="Batman"></Hero>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Superman"></Hero>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker"></Hero>
-      </ErrorBoundary>
+      <ClickCounter></ClickCounter>
+      <HoverCounter></HoverCounter>
     </div>
   );
 }
