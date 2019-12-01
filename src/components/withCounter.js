0c283a0f350bf1dde3b4cc9a460ed5acf89b42af
ruijadom <ruijadomingues@gmail.com>
2019-12-01 20:40:07 +0000

    changes functions names

--- src/components/withCounter.js
+++ src/components/withCounter.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const UpdatedComponent = OriginalComponent => {
-  class NewComponent extends React.Component {
+const withCounter = WrappedComponent => {
+  class WithCounter extends React.Component {
     constructor(props) {
       super(props);
 
@@ -18,14 +18,14 @@ const UpdatedComponent = OriginalComponent => {
 
     render() {
       return (
-        <OriginalComponent
+        <WrappedComponent
           count={this.state.count}
           incrementCount={this.incrementCount}
-        ></OriginalComponent>
+        ></WrappedComponent>
       );
     }
   }
-  return NewComponent;
+  return WithCounter;
 };
 
-export default UpdatedComponent;
+export default withCounter;
